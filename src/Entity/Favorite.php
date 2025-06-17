5e4dfb269f350b8bc1e0e058991feb4afbcf01eb
hamza <traorehamza407@gmail.com>
2025-06-17 12:33:18 +0200

    Ajout des entités Favorite et FavoriteRepository avec leurs propriétés et méthodes pour la gestion des favoris.

---
+++ src/Entity/Favorite.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\FavoriteRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: FavoriteRepository::class)]
+class Favorite
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column]
+    private ?\DateTimeImmutable $addedAt = null;
+
+    #[ORM\ManyToOne(inversedBy: 'favorites')]
+    private ?Room $room = null;
+
+    #[ORM\ManyToOne(inversedBy: 'favorites')]
+    private ?User $users = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getAddedAt(): ?\DateTimeImmutable
+    {
+        return $this->addedAt;
+    }
+
+    public function setAddedAt(\DateTimeImmutable $addedAt): static
+    {
+        $this->addedAt = $addedAt;
+
+        return $this;
+    }
+
+    public function getRoom(): ?Room
+    {
+        return $this->room;
+    }
+
+    public function setRoom(?Room $room): static
+    {
+        $this->room = $room;
+
+        return $this;
+    }
+
+    public function getUsers(): ?User
+    {
+        return $this->users;
+    }
+
+    public function setUsers(?User $users): static
+    {
+        $this->users = $users;
+
+        return $this;
+    }
+}
