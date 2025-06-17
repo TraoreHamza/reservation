1ae5cc6387c7f8b23e3a3c304d0140875aeadb2a
hamza <traorehamza407@gmail.com>
2025-06-17 12:33:11 +0200

    Ajout des entités Equipement et EquipementRepository avec leurs propriétés et méthodes pour la gestion des équipements.

---
+++ src/Entity/Equipement.php
@@ -0,0 +1,84 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\EquipementRepository;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: EquipementRepository::class)]
+class Equipement
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 60)]
+    private ?string $name = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $type = null;
+
+    /**
+     * @var Collection<int, Room>
+     */
+    #[ORM\ManyToMany(targetEntity: Room::class, inversedBy: 'equipements')]
+    private Collection $room;
+
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): static
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getType(): ?string
+    {
+        return $this->type;
+    }
+
+    public function setType(string $type): static
+    {
+        $this->type = $type;
+
+        return $this;
+    }
+
+    /**
+     * @return Collection<int, Room>
+     */
+    public function getRoom(): Collection
+    {
+        return $this->room;
+    }
+
+    public function addRoom(Room $room): static
+    {
+        if (!$this->room->contains($room)) {
+            $this->room->add($room);
+        }
+
+        return $this;
+    }
+
+    public function removeRoom(Room $room): static
+    {
+        $this->room->removeElement($room);
+
+        return $this;
+    }
+
+}
