8bb3553740194a50b9b273f4d7926bf29e217bf1
hamza <traorehamza407@gmail.com>
2025-06-17 15:03:55 +0200

    Ajout de la génération de données fictives dans LocationFixtures et suppression du code commenté pour une meilleure clarté

--- src/DataFixtures/LocationFixtures.php
+++ src/DataFixtures/LocationFixtures.php
@@ -2,15 +2,24 @@
 
 namespace App\DataFixtures;
 
-use Doctrine\Bundle\FixturesBundle\Fixture;
+use Faker\Factory;
+use App\Entity\Location;
 use Doctrine\Persistence\ObjectManager;
+use Doctrine\Bundle\FixturesBundle\Fixture;
 
 class LocationFixtures extends Fixture
 {
     public function load(ObjectManager $manager): void
     {
-        // $product = new Product();
-        // $manager->persist($product);
+        $faker = Factory::create('fr_FR');
+        $location = new Location();
+        $location
+            ->setCity($faker->city())
+            ->setDepartment($faker->department())
+            ->setNumber($faker->numberBetween(1, 100))
+            ->setState($faker->state())
+        ;
+        $manager->persist($location);
 
         $manager->flush();
     }
