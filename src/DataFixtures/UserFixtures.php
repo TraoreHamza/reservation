1fb071546291d14b568ee852d4b162982d9c807b
hamza <traorehamza407@gmail.com>
2025-06-17 16:12:12 +0200

    Correction de la génération de mots de passe dans UserFixtures et amélioration de la lisibilité du code

--- src/DataFixtures/UserFixtures.php
+++ src/DataFixtures/UserFixtures.php
@@ -12,17 +12,21 @@ class UserFixtures extends Fixture
     public function load(ObjectManager $manager): void
     {  
         $faker = Factory::create('fr_FR');
+
+        for ($i = 0; $i < 50; $i++) {
         $user = new User();
         $user
             ->setEmail($faker->email())
-            ->setPassword($this->hashPassword($user))
+            ->setPassword($faker->password())
             ->setWarning($faker->numberBetween(0, 1, 2, 3))
-            ->setIsBanned($faker->boolean())
-            ->setIsActive($faker->boolean())
+            ->setIsBanned($faker->boolean(56))
+            ->setIsActive($faker->boolean(75))
             ->setCreatedAt(new \DateTimeImmutable())
             ->setUpdatedAt(new \DateTimeImmutable())
         ;
         $manager->persist($user);
+        $this->addReference('user_' . $i, $user);
+    }
 
         $manager->flush();
     }
