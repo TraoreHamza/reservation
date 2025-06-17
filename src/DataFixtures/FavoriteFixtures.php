6499a51edea15412d283433481edc48400895992
hamza <traorehamza407@gmail.com>
2025-06-17 15:03:36 +0200

    Ajout de la génération de données fictives dans FavoriteFixtures et suppression du code commenté pour une meilleure clarté

--- src/DataFixtures/FavoriteFixtures.php
+++ src/DataFixtures/FavoriteFixtures.php
@@ -2,15 +2,21 @@
 
 namespace App\DataFixtures;
 
-use Doctrine\Bundle\FixturesBundle\Fixture;
+use Faker\Factory;
+use App\Entity\Favorite;
 use Doctrine\Persistence\ObjectManager;
+use Doctrine\Bundle\FixturesBundle\Fixture;
 
 class FavoriteFixtures extends Fixture
 {
     public function load(ObjectManager $manager): void
     {
-        // $product = new Product();
-        // $manager->persist($product);
+        $faker = Factory::create('fr_FR');
+        $favorite = new Favorite();
+        $favorite
+            ->setaddedAt(new \DateTimeImmutable())
+        ;
+        $manager->persist($favorite);
 
         $manager->flush();
     }
