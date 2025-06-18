a242c80876480aa56334cf6e148a98d169136232
hamza <traorehamza407@gmail.com>
2025-06-18 12:07:58 +0200

    Correction du nom de la classe 'EquipementFixtures' en 'EquipmentFixtures' pour uniformiser la casse.

--- src/DataFixtures/EquipementFixtures.php
+++ src/DataFixtures/EquipementFixtures.php
@@ -3,21 +3,21 @@
 namespace App\DataFixtures;
 
 use Faker\Factory;
-use App\Entity\Equipement;
+use App\Entity\Equipment;
 use Doctrine\Persistence\ObjectManager;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 
-class EquipementFixtures extends Fixture
+class EquipmentFixtures extends Fixture
 {
     public function load(ObjectManager $manager): void
     {
         $faker = Factory::create('fr_FR');
-        $equipement = new Equipement();
-        $equipement
+        $Equipment = new Equipment();
+        $Equipment
             ->setName($faker->word(2))
             ->setType($faker->word(2))
         ;
-        $manager->persist($equipement);
+        $manager->persist($Equipment);
 
         $manager->flush();
     }
