e3bbbbcf7a439f709b6d89823ea4315db39f7237
hamza <traorehamza407@gmail.com>
2025-06-20 11:31:26 +0200

    Mise à jour de LocationFixtures pour itérer sur les villes et départements, ajoutant des emplacements avec des données aléatoires.

--- src/DataFixtures/LocationFixtures.php
+++ src/DataFixtures/LocationFixtures.php
@@ -14,20 +14,130 @@ class LocationFixtures extends Fixture implements DependentFixtureInterface
     public function load(ObjectManager $manager): void
     {
         $faker = Factory::create('fr_FR');
+        $city = [
+            'Paris',
+            'Marseille',
+            'Lyon',
+            'Toulouse',
+            'Nice',
+            'Nantes',
+            'Strasbourg',
+            'Montpellier',
+            'Bordeaux',
+            'Lille',
+            'Rennes',
+            'Reims',
+            'Le Havre',
+            'Saint-Étienne',
+            'Toulon',
+            'Grenoble',
+            'Dijon',
+            'Angers',
+            'Nîmes',
+            'Villeurbanne',
+            'Saint-Denis',
+            'Aix-en-Provence',
+            'Clermont-Ferrand',
+            'Le Mans',
+            'Amiens',
+            'Tours',
+            'Limoges',
+            'Annecy',
+            'Perpignan',
+            'Boulogne-Billancourt',
+            'Metz',
+            'Besançon',
+            'Orléans',
+            'Saint-Denis (La Réunion)',
+            'Argenteuil',
+            'Rouen',
+            'Montreuil',
+            'Mulhouse',
+            'Caen',
+            'Nancy',
+            'Saint-Paul',
+            'Roubaix',
+            'Tourcoing',
+            'Nanterre',
+            'Avignon',
+            'Vitry-sur-Seine',
+            'Créteil',
+            'Poitiers',
+            'Aubervilliers',
+            'Versailles',
+        ];
+
+        $departements = [
+            'Ain',
+            'Aisne',
+            'Allier',
+            'Alpes-de-Haute-Provence',
+            'Hautes-Alpes',
+            'Alpes-Maritimes',
+            'Ardèche',
+            'Ardennes',
+            'Ariège',
+            'Aube',
+            'Aude',
+            'Aveyron',
+            'Bouches-du-Rhône',
+            'Calvados',
+            'Cantal',
+            'Charente',
+            'Charente-Maritime',
+            'Cher',
+            'Corrèze',
+            'Corse-du-Sud',
+            'Haute-Corse',
+            'Côte-d\'Or',
+            'Côtes-d\'Armor',
+            'Creuse',
+            'Dordogne',
+            'Doubs',
+            'Drôme',
+            'Eure',
+            'Eure-et-Loir',
+            'Finistère',
+            'Gard',
+            'Haute-Garonne',
+            'Gers',
+            'Gironde',
+            'Hérault',
+            'Ille-et-Vilaine',
+            'Indre',
+            'Indre-et-Loire',
+            'Isère',
+            'Jura',
+            'Landes',
+            'Loir-et-Cher',
+            'Loire',
+            'Haute-Loire',
+            'Loire-Atlantique',
+            'Loiret',
+            'Lot',
+            'Lot-et-Garonne',
+            'Lozère',
+            'Maine-et-Loire',
+        ];
+
+
         //Recuperation des room nouvellement crées
         $room = [];
         for ($i = 0; $i < 10; $i++) {
             $room[] = $this->getReference('ROOM_' . $i, Room::class);
         }
-        $location = new Location();
-        $location
-            ->setCity($faker->city())
-            ->setDepartment('')
-            ->setNumber($faker->numberBetween())
-            ->setState($faker->region())
-            ->addRoom($faker->randomElement($room))
-        ;
-        $manager->persist($location);
+
+        foreach ($city as $index => $cityName) {
+            $location = new Location();
+            $location
+                ->setCity($cityName)
+                ->setDepartment($departements[$index]) // Pour éviter l'index hors limites
+                ->setNumber($faker->numberBetween())
+                ->setState($faker->region())
+                ->addRoom($faker->randomElement($room))
+            ;
+            $manager->persist($location);
+        }
 
         $manager->flush();
     }
