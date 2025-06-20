b8dc098caec3ea6cde1424ca65bfe2bb6ef8db2e
hamza <traorehamza407@gmail.com>
2025-06-20 12:07:23 +0200

    Renommage de la propriété 'room' en 'rooms' dans la classe Option pour améliorer la cohérence et la clarté du code.

--- src/Entity/Option.php
+++ src/Entity/Option.php
@@ -22,7 +22,7 @@ class Option
      * @var Collection<int, Room>
      */
     #[ORM\ManyToMany(targetEntity: Room::class, inversedBy: 'options')]
-    private Collection $room;
+    private Collection $rooms;
 
     public function getId(): ?int
     {
@@ -46,13 +46,13 @@ class Option
      */
     public function getRoom(): Collection
     {
-        return $this->room;
+        return $this->rooms;
     }
 
     public function addRoom(Room $room): static
     {
-        if (!$this->room->contains($room)) {
-            $this->room->add($room);
+        if (!$this->rooms->contains($room)) {
+            $this->rooms->add($room);
         }
 
         return $this;
@@ -60,7 +60,7 @@ class Option
 
     public function removeRoom(Room $room): static
     {
-        $this->room->removeElement($room);
+        $this->rooms->removeElement($room);
 
         return $this;
     }
