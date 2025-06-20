203c98e18d79e4a9ca9f5f65c49f125629be398a
hamza <traorehamza407@gmail.com>
2025-06-20 10:15:37 +0200

    Ajout de l'interface DependentFixtureInterface à LocationFixtures et récupération des chambres nouvellement créées pour la gestion des emplacements.

--- src/DataFixtures/LocationFixtures.php
+++ src/DataFixtures/LocationFixtures.php
@@ -3,24 +3,38 @@
 namespace App\DataFixtures;
 
 use Faker\Factory;
+use App\Entity\Room;
 use App\Entity\Location;
 use Doctrine\Persistence\ObjectManager;
 use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Common\DataFixtures\DependentFixtureInterface;
 
-class LocationFixtures extends Fixture
+class LocationFixtures extends Fixture implements DependentFixtureInterface
 {
     public function load(ObjectManager $manager): void
     {
         $faker = Factory::create('fr_FR');
+        //Recuperation des room nouvellement crées
+        $room = [];
+        for ($i = 0; $i < 10; $i++) {
+            $room[] = $this->getReference('ROOM_' . $i, Room::class);
+        }
         $location = new Location();
         $location
             ->setCity($faker->city())
             ->setDepartment('')
             ->setNumber($faker->numberBetween())
             ->setState($faker->region())
+            ->addRoom($faker->randomElement($room))
         ;
         $manager->persist($location);
 
         $manager->flush();
+    }
+    public function getDependencies(): array
+    {
+        return [
+            RoomFixtures::class,
+        ];
     }
 }
